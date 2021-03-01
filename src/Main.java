d180fc9c2b72ac0767129e766c35aa7635997647
Vasiliy <60054664+zhmaylovv@users.noreply.github.com>
2021-03-01 19:18:17 +0700

    eq ok

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,16 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("Hello world");
+        int [] numbers = {10, 20, 30, 40, 50};
+
+        for(int x : numbers ){
+            System.out.print( x );
+            System.out.print(", ");
+        }
+        String st = "0100010011";
+        for (String i : st.split("")) {
+            if (i.equals("1"))
+                System.out.println(i);
+        }
     }
 }
