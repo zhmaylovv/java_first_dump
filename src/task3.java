b1cf0fa5ddc123be8fd66f5a4506370ccc64fe10
zhmaylovv <zhmaylovv@gmail.com>
2021-03-23 11:12:26 +0700

    whith try

--- src/task3.java
+++ src/task3.java
@@ -3,51 +3,39 @@ import java.util.*;
 
 public class Task3 {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+      double result = 0.0;
+      String[] exp_list;
+      String rawString = "";
+      Scanner input = new Scanner(System.in);
 
-        double result = 0.0;
-        String[] exp_list;
-        String rawString = "";
-        Scanner input = new Scanner(System.in);
+      System.out.println("Calculate simple expressions \"a + b\" format (\"q\" for quit)");
+      System.out.println("Enter an expression to calculate:");
 
-         while (!rawString.equalsIgnoreCase("q")){
-             System.out.println("Вычисление простых выражений. (\"q\" для выхода)");
-             System.out.println("Введите выражение для вычисления:");
-             rawString = input.nextLine();
-             if (rawString.contains(" ")){
-                 exp_list =  rawString.split(" ");
-                 switch (exp_list[1]){
-                     case "+": result = Double.parseDouble(exp_list[0]) + Double.parseDouble(exp_list[2]);
-                         break;
-                     case "-": result = Double.parseDouble(exp_list[0]) - Double.parseDouble(exp_list[2]);
-                         break;
-                     case "*": result = Double.parseDouble(exp_list[0]) * Double.parseDouble(exp_list[2]);
-                         break;
-                     case "/": result = Double.parseDouble(exp_list[0]) / Double.parseDouble(exp_list[2]);
-                         break;
-                     case "%": result = Double.parseDouble(exp_list[0]) % Double.parseDouble(exp_list[2]);
-                         break;
-                     default: result = 0.0;
-                 }
-             }else {
-                 exp_list =  rawString.split("[*/%+-]");
-                 if (rawString.contains("*")){
-                     result = Double.parseDouble(exp_list[0]) * Double.parseDouble(exp_list[1]);
-                 }else if (rawString.contains("/")){
-                     result = Double.parseDouble(exp_list[0]) / Double.parseDouble(exp_list[1]);
-                 }else if (rawString.contains("%")){
-                     result = Double.parseDouble(exp_list[0]) % Double.parseDouble(exp_list[1]);
-                 }else if (rawString.contains("+")){
-                     result = Double.parseDouble(exp_list[0]) + Double.parseDouble(exp_list[1]);
-                 }else if (rawString.contains("-")){
-                     result = Double.parseDouble(exp_list[0]) - Double.parseDouble(exp_list[1]);
-                 }
-
-
-             }
-
-
-            System.out.println("Результат: " + result + '\n');
-        }
-    }
+      while (true){
+          rawString = input.nextLine();
+              if (rawString.equalsIgnoreCase("q")){
+               break;
+              }
+          try {
+              exp_list =  rawString.split(" ");
+              if (exp_list[1].equals("*")){
+                  result = Double.parseDouble(exp_list[0]) * Double.parseDouble(exp_list[2]);
+              }else if (exp_list[1].equals("/")){
+                  result = Double.parseDouble(exp_list[0]) / Double.parseDouble(exp_list[2]);
+              }else if (exp_list[1].equals("%")){
+                  result = Double.parseDouble(exp_list[0]) % Double.parseDouble(exp_list[2]);
+              }else if (exp_list[1].equals("+")){
+                  result = Double.parseDouble(exp_list[0]) + Double.parseDouble(exp_list[2]);
+              }else if (exp_list[1].equals("*")){
+                  result = Double.parseDouble(exp_list[0]) - Double.parseDouble(exp_list[2]);
+              }
+          } catch(Exception e) {
+              System.out.println("Something went wrong, check the correctness" +
+                                  " of the input (a + b separated by a space)");
+          }
+          System.out.println("Result: " + result + "\n");
+          System.out.println("Enter next expression to calculate:");
+      }
+  }
 }
